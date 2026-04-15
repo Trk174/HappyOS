90e0a9c15e265f7805540a673f470842af01898c
MrKey <166393241+Trk174@users.noreply.github.com>
2026-04-15 17:38:37 +0300

    Add files via upload

---
+++ profiledef.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+iso_name="happyos"
+iso_label="HAPPYOS_$(date +%Y%m)"
+iso_publisher="HappyOS Project <https://happyos.com>"
+iso_application="HappyOS Live/Installation"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-ia32.grub.esp' 'uefi-x64.grub.esp' 'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86')
