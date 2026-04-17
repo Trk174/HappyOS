feca9577a67bba0b9580ff18f40edf4c4aacd457
MrKey <166393241+Trk174@users.noreply.github.com>
2026-04-17 15:42:11 +0300

    Update profiledef.sh

--- profiledef.sh
+++ profiledef.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-
-iso_name="happyos"
+iso_name="HappyOS"
 iso_label="HAPPYOS_$(date +%Y%m)"
-iso_publisher="HappyOS Project <https://happyos.com>"
-iso_application="HappyOS Live/Installation"
+iso_publisher="HappyOS Builder"
+iso_application="HappyOS Live/Rescue CD"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-ia32.grub.esp' 'uefi-x64.grub.esp' 'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
@@ -11,3 +9,10 @@ arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-resizepart"]="0:0:755"
+)
